4542917cf9921354c9c9aef43d9159f6be6c4054
Hamzaoutdoors <ellaouzihamza@gmail.com>
2022-01-12 20:34:00 +0100

    feat: Add a new category" page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'splashes#index'
   
-  resources :activities, only: [:index, :new, :show, :create]
   resources :categories, only: [:index, :new, :show, :create]
+  resources :activities, only: [:new, :show, :create]
 end
