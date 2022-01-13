37919d4c35c57e58342c3fe4fbd9204ffda5cb69
Hamzaoutdoors <ellaouzihamza@gmail.com>
2022-01-13 02:14:13 +0100

    feat: Add view for new transaction

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,8 +2,6 @@ class ActivitiesController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
 
-  def show; end
-
   def new
     @activity = Activity.new
   end
@@ -12,11 +10,12 @@ class ActivitiesController < ApplicationController
     @activity = current_user.activities.new(activity_params)
 
         if @activity.save
+          @activity_category = @activity.activity_categories.create(activity_category_params)
           flash[:notice] = 'activity was successfully created.'
         else
           flash[:alert] = "Failed to add activity - #{@activity.errors.full_messages.first}"
         end
-        redirect_to activities_path
+        redirect_to "/categories/#{@activity_category.category_id}"
   end
 
   def destroy
@@ -29,6 +28,10 @@ class ActivitiesController < ApplicationController
   end
 
   private
+
+  def activity_category_params
+    params.require(:activity).permit(:category_id)
+  end
 
   def activity_params
     params.require(:activity).permit(:name, :amount)
--- app/models/activity_category.rb
+++ app/models/activity_category.rb
@@ -1,4 +1,4 @@
-class ActionCategory < ApplicationRecord
+class ActivityCategory < ApplicationRecord
   belongs_to :activity
   belongs_to :category
 end
